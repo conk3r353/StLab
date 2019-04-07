64fe256d7796bd2903e7d3879b86176b0aeeeb26
Dmitriy Kharavets <kharavets123@mail.ru>
2019-04-07 23:44:48 +0300

    Fixed troubles with .gitignore

--- home7/main/processors.py
+++
@@ -1,5 +0,0 @@
-from main.models import Item
-
-
-def processor(request):
-    return {'ITEMS_AMOUNT': Item.objects.filter(is_sold=False).count()}
--- home7/main/templatetags/shop_urlify.py
+++
@@ -1,8 +0,0 @@
-from django import template
-
-register = template.Library()
-
-
-@register.filter(name='shop_urlify')
-def shop_urlify(shop):
-    return f'/shop/{shop.id}'
